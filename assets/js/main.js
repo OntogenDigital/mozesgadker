4926921e782afc5b143ef5bd85ccae4fcd74269d
Ryleejames <123148706+Ryleejames@users.noreply.github.com>
2023-02-28 21:17:32 +0530

    MZ

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+
 (function() {
   "use strict";
 
@@ -233,3 +234,45 @@
   });
 
 })()
+
+
+// popup
+
+$(document).ready(function(){
+
+  var stopAutohide;
+
+  function showWindow(){
+    $('#main').show();
+    // stop scroll
+    $('html body').css('overflow','hidden');
+    // auto hide fter 5s
+    stopAutohide = setTimeout(hideWindow,5000);
+
+  }
+  //showWindow()
+
+  function hideWindow(){
+    $('#main').hide();
+    // on scroll
+    $('html body').css('overflow','scroll');
+  }
+  //hideWindow()
+
+  // now call function automatically after some time 
+
+
+  // auto open after 2s
+  setTimeout(showWindow,100);
+
+  // close after click 
+
+  $("#close-btn").click(function(){
+
+    hideWindow();
+    celarTimeout(stopAutohide);
+
+  })
+
+
+})
